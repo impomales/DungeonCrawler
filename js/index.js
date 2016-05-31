257d5fb418c7451db683e53a5f830baf714ff0ff
impomales <impomales@gmail.com>
2016-05-30 23:32:43 -0400

    general algorithm for generating dungeon...Not entirely usable yet

--- js/index.js
+++ js/index.js
@@ -9,12 +9,14 @@
 // dimension of dungeon map.
 var width = 7;
 var height = 5;
+var threshold = 0.5 * width * height / 4;
 
 // room types.
 var SMALLROOM = { height: 2, width: 2 };
 var LARGEROOM = { height: 4, width: 4 };
 var VERTCORRIDOR = { height: 4, width: 2 };
 var HORIZCORRIDOR = { height: 2, width: 4 };
+var typesOfRooms = 4;
 
 // Dungeon map composed of any of the following.
 var FULL = 0; // space that is non occupiable.
@@ -39,10 +41,10 @@ function initDungeon(width, height) {
 }
 
 // creates a room at 2d-index of max width and height.
+// only creating l2r, t2b, should consider other cases.
 function createRoom(map, type, index) {
   for (var i = index[0]; i < index[0] + type.height; i++) {
     for (var j = index[1]; j < index[1] + type.width; j++) {
-      console.log("hello");
       map[i][j] = EMPTY;
     }
   }
@@ -50,16 +52,61 @@ function createRoom(map, type, index) {
 
 /* functions to be defined next time */
 // randomly selects a type of room.
-function decideRoom() {}
+function decideRoom() {
+  var selection = Math.floor(Math.random() * typesOfRooms);
+  switch (selection) {
+    case 0:
+      return SMALLROOM;
+    case 1:
+      return LARGEROOM;
+    case 2:
+      return VERTCORRIDOR;
+    case 3:
+      return HORIZCORRIDOR;
+    default:
+      return false;
+  }
+}
 
 // searches for a wall.
-function pickWall() {}
+function pickWall(map) {
+  var i, j;
+  do {
+    i = Math.floor(Math.random() * map.length);
+    j = Math.floor(Math.random() * map[i].length);
+  } while (!isWall(map, [i, j]));
+  return [i, j];
+}
 
 // checks if a new room fits at that index
-function doesFit() {}
+// this isn't complete, need to check if overlap.
+function doesFit(map, type, index) {
+  return index[0] + type.height < map.length && index[1] + type.width < map[i].length;
+}
 
 // dungeon generator
-function generateDungeon() {}
+function generateDungeon(width, height) {
+  // initialize map with EARTH cells.
+  map = initDungeon(width, height);
+
+  // create a room with center index.
+  var center = [map.length / 2 - SMALLROOM.height / 2, map[i].length / 2 - SMALLROOM.width / 2];
+  createRoom(map, SMALLROOM, center);
+
+  // create a new room until threshold is reached.
+  var numRooms = 1;
+  while (numRooms <= threshold) {
+    var success = false;
+    var type, index;
+    while (!success) {
+      index = pickWall(map);
+      type = decideRoom();
+      if (doesFit(map, type, index)) success = true;
+    }
+    createRoom(map, type, index);
+  }
+  return map;
+}
 
 // determines if current index is a wall.
 // wall is defined as any non full space adjacent to a full space.
@@ -80,7 +127,7 @@ var DungeonView = React.createClass({
 
   getInitialState: function getInitialState() {
     return {
-      map: initDungeon(width, height),
+      map: generateDungeon(width, height),
       health: 100,
       attack: 7,
       level: 0,
